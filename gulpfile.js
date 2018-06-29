fc38ccb28d3773366781c2728bb15dbe61bcb9ec
Phil Hawksworth <phil@netify.com>
2018-06-29 15:18:05 +0100

    git comands

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp  = require('gulp');
 
-
 /**
   Our gulp tasks live in their own files,
   for the sake of clarity.
@@ -12,6 +11,8 @@ require('require-dir')('./gulp-tasks');
   Let's gwt the data we need and then build this sucker.
 */
 gulp.task('build', gulp.series(
-  'test:stage'
-  // 'generate'
+  // 'test:stage'
+  'generate'
 ));
+
+
