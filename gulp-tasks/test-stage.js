d0cfa850c425e5c19e336cd9798c4616dd90a8f3
Phil Hawksworth <phil@netify.com>
2018-06-29 15:06:26 +0100

    logging git

--- gulp-tasks/test-stage.js
+++ gulp-tasks/test-stage.js
@@ -1,17 +1,34 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
-const simpleGit = require('simple-git')('../');
+// const simpleGit = require('simple-git')('../');
 
 /*
  Run our static site generator to build the pages
 */
-gulp.task('test:stage', shell.task('eleventy'));
+// gulp.task('test:stage', shell.task('eleventy'));
 
 
 
 
 gulp.task('test:stage', function() {
-  simpleGit.branch(function(b){
-    console.log(`{b}`);
-  })
+  // simpleGit.branch(function(b){
+  //   console.log(`{b}`);
+  // })
+
+
+  require('simple-git')(__dirname)
+  .pull()
+  .tags((err, tags) => console.log("Latest available tag: %s", tags.latest));
+
+// update repo and when there are changes, restart the app
+// require('simple-git')()
+//   .pull((err, update) => {
+//      if(update && update.summary.changes) {
+//         require('child_process').exec('npm restart');
+//      }
+//   });
+
+
 });
+
+
